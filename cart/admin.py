7a8c6677daa7770029aa0bc9b94fa4e742242312
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 18:56:33 +0500

    update admin panel

--- cart/admin.py
+++ cart/admin.py
@@ -1,20 +1,57 @@
+from cart.models import CartDish
+from cart.models import Cart
+
 from django.contrib import admin
 
-from cart.models import Cart, CartDish
-from profiles.models import ClientProfile
+
+class CartDishInline(admin.TabularInline):
+    model = CartDish
+
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'profile',)
-    list_display_links = ('id', 'profile',)
+    inlines = [
+        CartDishInline,
+    ]
+    list_display = (
+        "id",
+        "profile",
+    )
+    search_fields = (
+        "profile__name",
+        "profile__surname",
+        "profile__father_name",
+    )
+    list_display_links = (
+        "id",
+        "profile",
+    )
 
 
 @admin.register(CartDish)
 class CartDishAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'cart', 'dish', 'quantity',)
-    list_display_links = ('id', 'cart', 'dish', 'quantity',)
-    # search_fields = ()
-    list_filter = ('cart', 'dish', 'quantity',)
-    ordering = ('id',)
+    list_display = (
+        "id",
+        "cart",
+        "dish",
+        "quantity",
+    )
+    list_display_links = (
+        "id",
+        "cart",
+        "dish",
+        "quantity",
+    )
+    search_fields = (
+        "cart__profile__name",
+        "cart__profile__surname",
+        "cart__profile__father_name",
+    )
+    list_filter = (
+        "cart",
+        "dish",
+        "quantity",
+    )
+    ordering = ("id",)
