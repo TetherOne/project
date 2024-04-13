0fd984b886be625165ebece67814fb78ec099d14
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 18:56:20 +0500

    update admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -5,4 +5,37 @@ from django.contrib import admin
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("text",)
+
+    list_display = (
+        "id",
+        "short_text",
+        "dish",
+        "author",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "text",
+        "dish__name",
+        "author__name",
+        "author__surname",
+        "author__father_name",
+    )
+    list_display_links = (
+        "id",
+        "short_text",
+        "dish",
+        "author",
+    )
+    list_filter = (
+        "dish",
+        "author__name",
+        "author__surname",
+        "author__father_name",
+    )
+    ordering = ("id",)
+
+    def short_text(self, obj):
+        if len(obj.text) > 40:
+            return obj.text[:40] + "..."
+        return obj.text
