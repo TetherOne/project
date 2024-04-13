313b03a6c82c1e2ace93ec8a8cdc095dafefd450
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 17:32:24 +0500

    update admin classes

--- cart/admin.py
+++ cart/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 
-from cart.models import Cart
-
+from cart.models import Cart, CartDish
+from profiles.models import ClientProfile
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
 
-    list_display = (
-        "profile",
-        "dish",
-        "count",
-    )
+    list_display = ('id', 'profile',)
+    list_display_links = ('id', 'profile',)
+
+
+@admin.register(CartDish)
+class CartDishAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'cart', 'dish', 'quantity',)
+    list_display_links = ('id', 'cart', 'dish', 'quantity',)
+    # search_fields = ()
+    list_filter = ('cart', 'dish', 'quantity',)
+    ordering = ('id',)
