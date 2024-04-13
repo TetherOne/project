7cfe24fda8316aab4afcdbb19d53863240c285b8
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 17:33:22 +0500

    ad cartdish serializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from cart.models import Cart
+from cart.models import Cart, CartDish
 
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
+        fields = "__all__"
+
+
+class CartDishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartDish
         fields = "__all__"
