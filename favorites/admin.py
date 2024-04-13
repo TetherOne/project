77f665a21f470c31ec784b4cccf6735b911e9205
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 18:10:06 +0500

    update admin panel

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,16 +1,26 @@
-from django.contrib import admin
-
 from favorites.models import Favorites
+
+from django.contrib import admin
 
 
 @admin.register(Favorites)
 class FavoritesAdmin(admin.ModelAdmin):
 
     list_display = (
+        "id",
         "profile",
         "dish",
     )
-    list_display_links = ("profile",)
-    search_fields = ("profile",)
-    list_filter = ("profile",)
+    list_display_links = (
+        "profile",
+        "dish",
+    )
+    search_fields = (
+        "profile__name",
+        "dish__name",
+    )
+    list_filter = (
+        "profile",
+        "dish",
+    )
     ordering = ("id",)
