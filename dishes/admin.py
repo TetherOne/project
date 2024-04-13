cafb2bc9e46d484a7e7c48b04bfd9048157e3ab8
Pavel Ryaguzov <pashajobber@gmail.com>
2024-04-13 18:18:43 +0500

    update admin panel

--- dishes/admin.py
+++ dishes/admin.py
@@ -8,23 +8,54 @@ from django.contrib import admin
 class DishAdmin(admin.ModelAdmin):
 
     list_display = (
+        "id",
         "name",
         "description",
+        "get_category_name",
         "price",
     )
     list_display_links = ("name",)
-    search_fields = ("name",)
-    list_filter = (
+    search_fields = (
         "name",
+        "description",
+        "category__name",
         "price",
     )
-    ordering = ("id",)
+    list_filter = (
+        "category",
+        "price",
+    )
+    ordering = (
+        "id",
+        "name",
+        "price",
+        "category",
+    )
+
+    def get_category_name(self, obj):
+        return obj.category.name
+
+    get_category_name.short_description = "Category"
 
 
 @admin.register(DishCategory)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    list_display_links = ("name",)
-    search_fields = ("name",)
-    list_filter = ("name",)
+
+    list_display = (
+        "id",
+        "name",
+        "dish_count",
+    )
+    list_display_links = (
+        "id",
+        "name",
+    )
+    search_fields = (
+        "id",
+        "name",
+    )
+    list_filter = (
+        "name",
+        "dish_count",
+    )
     ordering = ("id",)
